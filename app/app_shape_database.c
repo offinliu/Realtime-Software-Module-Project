9d223226317d2b791297994ea0929aac7e6f06f5
KeeJin <yoplocheo@gmail.com>
2021-10-05 17:44:21 +0800

    release main linked list's memory before program termination

--- app/app_shape_database.c
+++ app/app_shape_database.c
@@ -5,5 +5,9 @@ int main(int argc, char* argv[]) {
   file_path = (char*)"test";
   LinkedList main_list;
   while (MainMenu(&main_list, file_path));
+  
+	if(freemem(&main_list) !=0) {
+    printf("Warning: memory free failed.");
+  }
   return 0;
 }
